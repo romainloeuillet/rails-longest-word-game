714858eb00b3c265fb47b5afd53dfac96665ef98
Romain Loeuillet <loeuillet.romain@gmail.com>
2018-08-13 16:03:28 +0200

    done 2

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,31 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    attempt = params[:word].chars.map { |letter| params[:letters].include?(letter.upcase) }
+    if attempt.include?(true)
+      if english_word?
+        @score = "The word is valid according to the grid and is an English word"
+      else
+        @score = "The word is valid according to the grid, but is not a valid English word"
+      end
+    else
+      @score = "The word can't be built out of the original grid"
+    end
+  end
+
+  private
+
+  def english_word?
+    response = open("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+end
